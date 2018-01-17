1a61840ca313ea4c4bb0e39c3de53de90e668837
stephencisler <stephencisler@gmail.com>
2018-01-17 10:13:51 -0600

    project made. math controller built

---
+++ src/main/java/com/codeup/springbootblog/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.springbootblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable String num1, @PathVariable String num2) {
+        return Integer.toString(Integer.parseInt(num1) + Integer.parseInt(num2));
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable String num1, @PathVariable String num2) {
+        return Integer.toString(Integer.parseInt(num2) - Integer.parseInt(num1));
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable String num1, @PathVariable String num2) {
+        return Integer.toString(Integer.parseInt(num1) * Integer.parseInt(num2));
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable String num1, @PathVariable String num2) {
+        return Float.toString(Float.parseFloat(num1) / Float.parseFloat(num2));
+    }
+}
