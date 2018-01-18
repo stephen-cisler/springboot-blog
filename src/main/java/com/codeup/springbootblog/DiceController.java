0d304a95fb0eca6aada56b5cd64f49bad57b0a0e
stephencisler <stephencisler@gmail.com>
2018-01-18 00:01:57 -0600

    roll-dice section complete

--- src/main/java/com/codeup/springbootblog/DiceController.java
+++ src/main/java/com/codeup/springbootblog/DiceController.java
@@ -12,15 +12,17 @@ public class DiceController {
 
     @GetMapping("/roll-dice")
     public String dicePage(Model viewModel) {
+        System.out.println("roll dice main");
         viewModel.addAttribute("rollPick", true);
         return "diceRoll";
     }
 
     @GetMapping("/roll-dice/{guess}")
     public String diceGuess(@PathVariable String guess, Model viewModel) {
+        System.out.println("roll dice guesses");
         int roll = new Random().nextInt(6 - 1) +1;
         boolean correctGuess = (roll == Integer.parseInt(guess));
-        viewModel.addAttribute("rollPick", true);
+        viewModel.addAttribute("rollPick", false);
         viewModel.addAttribute("correctGuess", correctGuess);
         viewModel.addAttribute("guess", guess);
         viewModel.addAttribute("roll", roll);
