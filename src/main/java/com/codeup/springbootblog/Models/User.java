723497b4baa592dc1ee1a1ee6e4503a0793a1ba5
stephencisler <stephencisler@gmail.com>
2018-01-24 10:16:31 -0600

    User and Post mapped to each other

--- src/main/java/com/codeup/springbootblog/Models/User.java
+++ src/main/java/com/codeup/springbootblog/Models/User.java
@@ -1,10 +1,12 @@
 package com.codeup.springbootblog.Models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="users")
 public class User {
+
     @Id @GeneratedValue
     private long id;
     @Column(nullable = false)
@@ -13,4 +15,46 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Post> posts;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 }
