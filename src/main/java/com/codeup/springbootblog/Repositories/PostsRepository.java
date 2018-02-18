6acad45437dbac325904912baba0e7ab28453d8a
stephencisler <stephencisler@gmail.com>
2018-02-17 23:02:14 -0600

    added posts repository

--- src/main/java/com/codeup/springbootblog/Repositories/PostsRepository.java
+++ src/main/java/com/codeup/springbootblog/Repositories/PostsRepository.java
@@ -4,4 +4,6 @@ import com.codeup.springbootblog.Models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostsRepository extends CrudRepository<Post, Long> {
+
+
 }
