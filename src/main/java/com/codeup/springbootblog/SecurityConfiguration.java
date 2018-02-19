20f86135ffbefb825bdddaf519e8ac9958903dba
stephencisler <stephencisler@gmail.com>
2018-02-19 13:32:39 -0600

    added new antMatchers to register

--- src/main/java/com/codeup/springbootblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springbootblog/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/posts") // anyone can see the home and the ads pages
+                .antMatchers("/", "/posts", "/sign-up") // anyone can see the home and the ads pages
                 .permitAll()
             /* Pages that require athentication */
                 .and()
